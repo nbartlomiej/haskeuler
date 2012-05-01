bca99a4e1354f3e44ca197079bc437858cc192f8
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2012-05-01 14:12:16 +0200

    Problem 007 draft

---
+++ 007/problem-007.hs
@@ -0,0 +1,9 @@
+-- Needs improvement, using external Primes library.
+
+import Data.List
+import Data.Numbers.Primes
+
+result :: Int
+result = primes !! 10000 -- Not 10001 as we start counting from zero.
+
+main = putStrLn $ show(result)
