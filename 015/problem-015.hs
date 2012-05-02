49cbf96677992a960f4a332601d010ba01777b72
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2012-05-02 20:57:36 +0200

    Problem 015, using better algorithm

--- 015/problem-015.hs
+++ 015/problem-015.hs
@@ -1,38 +1,12 @@
-import Data.List
-
--- Let's not use the concept of paths and consider fields instead. Fields on a
--- board can be either free or occupied. Assume that path always sticks to the
--- top right borders of free fields. E.g. the examples in ProjectEuler's
--- problem description correspond to the following "field representation":
---   0 0   0 1   0 1   1 1   1 1   1 1
---   0 0 , 0 0 , 0 1 , 0 0 , 0 1 , 1 1
---
--- Let's reduce that to a count of occupied fields per column (starting from
--- top):
---   [0.0], [0,1], [0,2], [1,1] ,[1,2], [2,2]
---
--- The following function will return number of possible field permutations for
--- a board of given length (this number of field permutations is equal to
--- number of paths).
-fieldsCount :: Int -> Int
-fieldsCount width = length $ fields width
-
-fields :: Int -> [[Int]]
-fields width = (unfoldr next' (take width $ repeat 0)) ++ full
+-- Pascal triangle function adapted from:
+-- http://www.haskell.org/haskellwiki/Blow_your_mind
+pascal :: [[Int]]
+pascal = (iterate generateRow [1])
   where
-    next' :: [Int] -> Maybe( [Int], [Int])
-    next' fields = if (last fields == width)
-      then Nothing
-      else Just (fields, increment fields width)
-    full = [take width $ repeat width]
+    generateRow :: [Int] -> [Int]
+    generateRow row = zipWith (+) ([0] ++ row) (row ++ [0])
 
-increment :: [Int] -> Int -> [Int]
-increment [] base = []
-increment (x:xs) base = if x < base
-    then (x+1):xs
-    else
-      let incremented = increment xs base
-          duplicateFirst (y:ys) = (y:y:ys)
-      in duplicateFirst incremented
+result :: Int -> Int
+result n = pascal !! (n*2) !! n
 
-main = putStrLn $ show $ fieldsCount 20
+main = putStrLn $ show $ result 20
